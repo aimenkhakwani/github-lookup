de6bb20c8b33ad1bf4ec0f816132ccd2c46df4c0
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-10-14 12:56:48 -0700

    Add an image of the users profile

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,17 +1,16 @@
 var Github = require('./../js/github.js').githubModule;
 
-// Book.prototype.listBooks = function(books) {
-//   $('.all_books').empty();
-//   for (var i = 0; i < books.length; i++) {
-//     $('.all_books').append("<br><div class='row'><div class='col-sm-3'>" + '<img src="' + books[i][4] + '">' + "</div>" + "<div class='col-sm-8 col-sm-offset-1'><h3>" + books[i][0] + "</h3><br>" + "<p class='bold'>Author(s): " + books[i][1] + "</p><p class='bold'>Description: " + books[i][2] + "</p><p class='bold'>Average Rating: "
-//     + books[i][3] + "</p></div></div><br>");
-//   }
-// }
-
 var displayUser = function(info, repos) {
-  $('.output').empty();
-  $('.col-md-3').append("<img src='" + info[4] + "'></div>");
-  $('.col-md-4').append("<p>Username: " + info[0] + "<br>Name: " + info[1] + "<br>Location: " + info[2] + "<br>Bio: " + info[3] + "<br>Followers: " + info[5] + "<br>Folowing: " + info[6] + "</p>");
+  $('h2').show();
+  $('#user-info').empty();
+  $('#user-repos').empty();
+  $('#user-info').append("<br><br><div class='col-md-3 col-md-offset-3'><img src='" + info[3] + "'></div>" + "<div class='col-md-3'><h3>" + info[0] + "</h3><p>" + info[1] + "<br>" + info[2] +  "</p></div>");
+  for (var i = 0; i < repos.length; i++) {
+    if(repos[i][1] === null){
+      repos[i][1] = "";
+    }
+    $('#user-repos').append("<br><div class='col-md-10 col-md-offset-1 well'><h4>" + repos[i][0] + "</h4><h5>" + repos[i][1] + "</h5></div>");
+  };
 };
 
 $(document).ready(function() {
--- js/github.js
+++ js/github.js
@@ -1,7 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 
 function Github() {
-
 }
 
 Github.prototype.getUser = function(username, displayFunction) {
@@ -12,14 +11,11 @@ Github.prototype.getUser = function(username, displayFunction) {
     var user = response.login;
     var name = response.name;
     var location = response.location;
-    var bio = response.bio;
     var image = response.avatar_url;
-    var followers = response.followers;
-    var following = response.following;
-    info.push(user, name, location, bio, image, followers, following);
-    // second get request to get user repos 
+    info.push(user, name, location, image);
+
+    // second get request to get user repos
     $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(result) {
-      // console.log(result);
       var repos = result;
       repos.forEach(function(repo) {
         var repoName = repo.name;
@@ -29,17 +25,6 @@ Github.prototype.getUser = function(username, displayFunction) {
       displayFunction(info, repoInfo);
     });
   });
-
-  // $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(result) {
-  //   // console.log(result);
-  //   var repos = result;
-  //   repos.forEach(function(repo) {
-  //     var repoName = repo.name;
-  //     var description = repo.description;
-  //     repoInfo.push([repoName, description]);
-  //   });
-  //   displayFunction(repoInfo);
-  // });
 };
 
 exports.githubModule = Github;
