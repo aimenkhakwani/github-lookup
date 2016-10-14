83375e91f3c13a4ea3bba9a10e025e5210b931c3
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-10-14 09:36:34 -0700

    Add a second get request in the same prototype to get repos for that user

--- js/github.js
+++ js/github.js
@@ -5,6 +5,8 @@ function Github() {
 }
 
 Github.prototype.getUser = function(username) {
+  var info = [];
+  var repoInfo = [];
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
     var user = response.login;
     var name = response.name;
@@ -13,11 +15,31 @@ Github.prototype.getUser = function(username) {
     var image = response.avatar_url;
     var followers = response.followers;
     var following = response.following;
-    console.log(location);
-    console.log(response);
+    info.push(user, name, location, bio, image, followers, following)
+    // console.log(info);
   }).fail(function(error){
     console.log(error.response.JSON.message);
   });
-}
+
+  $.get('https://api.github.com/users/' + username + '/repos').then(function(result) {
+    var repos = result;
+    console.log(repos);
+    repos.forEach(function(repo) {
+      var repoName = repo.name;
+      var description = repo.description;
+      // console.log(description);
+    });
+  }).fail(function(fail){
+    console.log(fail.response.JSON.message);
+  });
+};
+
+// Github.prototype.getUserRepos = function(username) {
+//   $.get('https://api.github.com/users/' + username + '/repos').then(function(response1) {
+//     console.log(response1);
+//   }).fail(function(error){
+//     console.log(error.response.JSON.message);
+//   });
+// }
 
 exports.githubModule = Github;
